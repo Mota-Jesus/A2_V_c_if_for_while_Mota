645b5066284090c3460f23d2df464a32c4df2883
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2024-09-06 09:48:30 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+'''Jesus Mota
+Trabajando con cadenas
+'''
+Nombre="Jesus"
+print(Nombre[0])
+print(Nombre)
+
+MisNovias=["a","e","u"]
+for chicas in MisNovias:
+    print(chicas)
+    
+    print("Otro ejemplo")
+    b="Jesus Mota"
+print(b[2 : 5])
+print("a mayuscula")
+a = "Mota"
+print(a.upper())
+print("Concatenar")
+a= "Roger "
+b="Berenjena"
+c= a + b
+print(c)
---
+++ misvariables.py
@@ -0,0 +1,18 @@
+import math
+edad=16 #16 se guarda en edad
+nombre="Jesus Mota"
+
+print("tu edad es ",edad)
+print("te llamas ",nombre)
+
+print("-- tipo de variables ----")
+x = str(7) # x will be '3'
+y = int(3.1415) # y will be 3
+z = float(7) # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+print("-- Funciones matematicas ----")
+x = math.sqrt(64)
+print(x)
